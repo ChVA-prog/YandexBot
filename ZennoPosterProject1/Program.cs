c49339d5c44078f1bd5b8be13c6d3e4fb5952501
CherepanovV <slavcherepano@yandex.ru>
2022-06-17 09:21:36 +0300

    Тест

--- ZennoPosterProject1/Program.cs
+++ ZennoPosterProject1/Program.cs
@@ -43,13 +43,8 @@ namespace ZennoPosterProject1
         public int Execute(Instance instance, IZennoPosterProjectModel project)
         {
             int executionResult = 0;
-            project.SendInfoToLog("Это сборка из ветки FeedCookies", true);
-            project.SendInfoToLog("Считываем входные настройки", true);
-            new InputSettings(instance, project).InitializationInputValue();
 
-            project.SendInfoToLog("Запускаем нагуливание кук.", true);
-            new StartMethod(instance, project).FeedingCookies();
-            project.SendInfoToLog("Закончили.");
+
 
             return executionResult;
         }
