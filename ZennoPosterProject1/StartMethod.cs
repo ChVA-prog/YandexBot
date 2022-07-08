41f930d8cfc4e047c81ce984c290a14464909127
CherepanovV <slavcherepano@yandex.ru>
2022-07-08 21:56:53 +0300

    Удалил лишнее из ветки куки

--- ZennoPosterProject1/StartMethod.cs
+++ ZennoPosterProject1/StartMethod.cs
@@ -4,7 +4,6 @@ using ZennoPosterDataBaseAndProfile;
 using ZennoPosterProxy;
 using ZennoPosterYandexWalk;
 using System;
-using ZennoPosterYandexRegistration;
 
 
 namespace ZennoPosterProject1
@@ -62,51 +61,6 @@ namespace ZennoPosterProject1
             proxyDB.ChangeIp();
             proxyDB.ChangeStatusProxyInDB("Free");
             profile.UpdateStatusProfile("Free", 1, 1);
-        }//Нагуливание кук
-        public void YandexRegistration()
-        {
-            Profile profile = new Profile(project);
-            DBMethods dBMethods = new DBMethods(instance, project);
-            ProxyDB proxyDB = new ProxyDB(instance, project);
-
-            try
-            {
-                dBMethods.DownloadProfileInZennoposter();
-            }
-            catch (Exception ex)
-            {               
-                project.SendErrorToLog(ex.Message,true);
-                throw new Exception(ex.Message);
-            }//Загрузка профиля в проект.                      
-            try
-            {
-                proxyDB.SetProxyInInstance();
-            }
-            catch (Exception ex)
-            {
-                project.SendErrorToLog(ex.Message, true);
-                dBMethods.UpdateStatusProfile("Free");
-                throw new Exception(ex.Message);
-            }//Установка прокси в инстанс.
-            try
-            {
-                new RegistrationAndSettingsAccount(instance, project).RegisterAccountAndSetPassword();
-                new RegistrationAndSettingsAccount(instance, project).SetLoginAndPasswordAndRemovePhoneNumber();
-                new RegistrationAndSettingsAccount(instance, project).DeletePhoneNumberFromAccount();
-            }
-            catch (Exception ex)
-            {
-                project.SendErrorToLog(ex.Message);
-                profile.SaveProfile();
-                proxyDB.ChangeIp();
-                proxyDB.ChangeStatusProxyInDB("Free");
-                dBMethods.UpdateStatusProfile("Free");
-                throw new Exception(ex.Message);
-            }//Регистрируем аккаунт, устанавливаем логин с паролем, отвязываем номер.
-            profile.SaveProfile();
-            proxyDB.ChangeIp();
-            proxyDB.ChangeStatusProxyInDB("Free");
-            dBMethods.UpdateStatusProfile("Free", "YES");
-        }//Регистрация в яндексе и отвязка номера
+        }//Нагуливание кук      
     }
 }
