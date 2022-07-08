2eb2cb49a4bf2c56ab324c512a35ecc4486a5c83
CherepanovV <slavcherepano@yandex.ru>
2022-07-08 16:22:42 +0300

    Начал делать разгадывание капчи

--- ZennoPosterProject1/Program.cs
+++ ZennoPosterProject1/Program.cs
@@ -25,6 +25,37 @@ namespace ZennoPosterProject1
             project.SendInfoToLog("Считываем входные настройки", true);
             new InputSettings(instance, project).InitializationInputValue();
 
+
+            //SwipeAndClick swipeAndClick = new SwipeAndClick(instance,project);
+
+            //HtmlElement IAmNotRobot = instance.ActiveTab.FindElementByXPath("//span[contains(text(),'похожи на автоматические')]", 0);
+            //if (!IAmNotRobot.IsVoid)
+            //{
+            //    swipeAndClick.ClickToElement(instance.ActiveTab.FindElementByXPath("//div[contains(@class,'CheckboxCaptcha')]", 0));
+            //}
+
+            //HtmlElement InpuCaptcha = instance.ActiveTab.FindElementByXPath("//label[contains(text(),'Введите текст с картинки')]", 0);
+            //if (!InpuCaptcha.IsVoid)
+            //{
+            //    HtmlElement InputTextCaptcha = instance.ActiveTab.FindElementByXPath("//input[contains(@class,'Textinput-Control')]", 0);
+            //    HtmlElement Captcha = instance.ActiveTab.FindElementByXPath("//img[contains(@class,'AdvancedCaptcha')]", 0);
+            //    string CaptchaImgUrl = Captcha.GetAttribute("src");
+            //    project.SendInfoToLog(CaptchaImgUrl);
+
+
+
+
+            //}
+
+            //string url = "https://ext.captcha.yandex.net/image?key=00AfT1S8cXo2ulreugGAtM00qkxwgHHt";
+            //// Отправить на распознавание
+            //var goga = ZennoPoster.CaptchaRecognition("RuCaptcha.dll", url, "");
+
+            //project.SendInfoToLog(goga);
+
+
+
+
             StartMethod startMethod = new StartMethod(instance, project);
 
             try
