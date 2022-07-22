82f3c951597f9a45a8ea9315572b2017cb1436d6
CherepanovV <slavcherepano@yandex.ru>
2022-07-22 17:16:24 +0300

    Сделал пути xpath входными настройками проекта

--- ZennoPosterProject1/YandexWalk/YandexWalkValue.cs
+++ ZennoPosterProject1/YandexWalk/YandexWalkValue.cs
@@ -20,6 +20,21 @@ namespace ZennoPosterYandexWalk
         protected string PageCountSearch { get; set; }
         protected string CountLearnCardIn { get; set; }
         protected string CountGetCard { get; set; }
+        protected string HtmlElementYandexTrashAlisa { get; set; }
+        protected string HtmlElementYandexTrashKinopoisk { get; set; }
+        protected string HtmlElementYandexTrashDzen { get; set; }
+        protected string HtmlElementYandexTrashYandexMarket { get; set; }
+        protected string HtmlElementYandexTrashYandexMarket2 { get; set; }
+        protected string HtmlElementYandexTrashYandexBrowser { get; set; }
+        protected string HtmlElementPageNumberDie { get; set; }
+        public string HtmlElementPageIamNotRobot { get; set; }
+        public string HtmlElementIAmNotRobot { get; set; }
+        public string HtmlElementInputCapcha { get; set; }
+        public string HtmlElementPageCapcha { get; set; }
+        public string HtmlElementSendCapcha { get; set; }
+        public string HtmlElementCapchaImage { get; set; }
+        public string HtmlElementCapchaError { get; set; }
+        
         protected List<string> MyUrlList { get; set; }
         public YandexWalkValue(IZennoPosterProjectModel _project)
         {
@@ -35,8 +50,22 @@ namespace ZennoPosterYandexWalk
             PageCountSearch = Project.Variables["set_PageCountSearch"].Value;
             CountLearnCardIn = Project.Variables["set_CountLearnCard"].Value;
             CountGetCard = Project.Variables["set_CountGetCard"].Value;
+            HtmlElementYandexTrashAlisa = Project.Variables["set_HtmlElementYandexTrashAlisa"].Value;
+            HtmlElementYandexTrashKinopoisk = Project.Variables["set_HtmlElementYandexTrashKinopoisk"].Value;
+            HtmlElementYandexTrashDzen = Project.Variables["set_HtmlElementYandexTrashDzen"].Value;
+            HtmlElementYandexTrashYandexMarket = Project.Variables["set_HtmlElementYandexTrashYandexMarket"].Value;
+            HtmlElementYandexTrashYandexMarket2 = Project.Variables["set_HtmlElementYandexTrashYandexMarket2"].Value;
+            HtmlElementYandexTrashYandexBrowser = Project.Variables["set_HtmlElementYandexTrashYandexBrowser"].Value;
+            HtmlElementPageNumberDie = Project.Variables["set_HtmlElementPageNumberDie"].Value;
+            HtmlElementPageIamNotRobot = Project.Variables["set_HtmlElementPageIamNotRobot"].Value;
+            HtmlElementIAmNotRobot = Project.Variables["set_HtmlElementIAmNotRobot"].Value;
+            HtmlElementInputCapcha = Project.Variables["set_HtmlElementInputCapcha"].Value;
+            HtmlElementPageCapcha = Project.Variables["set_HtmlElementPageCapcha"].Value;
+            HtmlElementSendCapcha = Project.Variables["set_HtmlElementSendCapcha"].Value;
+            HtmlElementCapchaImage = Project.Variables["set_HtmlElementCapchaImage"].Value;
+            HtmlElementCapchaError = Project.Variables["set_HtmlElementCapchaError"].Value;
             MyUrlList = Project.Variables["set_MyUrl"].Value.Split(new string[] { "\r\n" }, StringSplitOptions.None).ToList();
-            Program.logger.Debug("Закончили считывать настройки для YandexWalk.");
+                Program.logger.Debug("Закончили считывать настройки для YandexWalk.");
         }
     }
 }
